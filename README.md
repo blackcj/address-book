410fa44ec8137e56ad4cb17c66e3c0c6a66d596c
Christopher Black <chris@blacktopinteractive.com>
2019-05-04 20:23:23 -0500

    Added stack navigator to allow for switching between screens. Moved the search input and submit button into a separate component.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Setup
 
-Follow the *React Native CLI Quickstart* prerequisite instructions located [here](https://facebook.github.io/react-native/docs/getting-started). You will need Node, Watchman, React Native command line interface, Xcode, JDK 8, Android Studio and the Android SDK. 
+Follow the *React Native CLI Quickstart* prerequisite instructions located [here](https://facebook.github.io/react-native/docs/getting-started). You will need Node, TypeScript, Watchman, React Native command line interface, Xcode, JDK 8, Android Studio and the Android SDK. 
 
 After completing the quick start guide, use the following commands to start up the app on your emulator or device:
 
---
+++ components/SearchScreen.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+const instructions = Platform.select({
+    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+    android:
+        'Double tap R on your keyboard to reload,\n' +
+        'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+class SearchScreen extends Component<Props> {
+    state = { address: '123 Street', search: '' };
+
+    handleSubmit = () => {
+        const searchString = `You entered: ${this.state.address}`;
+        this.setState({ search: searchString });
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Enter an Address:</Text>
+                <TextInput
+                    style={styles.addressInput}
+                    onChangeText={(address) => this.setState({ address })}
+                    value={this.state.address} />
+                <Button
+                    onPress={this.handleSubmit}
+                    title="Submit"
+                    color="#841584"
+                    accessibilityLabel="Submit" />
+                <Text style={styles.instructions}>{this.state.search}</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    addressInput: {
+        height: 40,
+        minWidth: 200,
+        borderColor: 'gray',
+        borderWidth: 1,
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+        margin: 10,
+    },
+});
+
+export default SearchScreen;
